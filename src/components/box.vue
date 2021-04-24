0264bed0e4605f4a13b829dc330e1cfe06e4d7e4
Dias Baiseit <dias.baiseit.02@gmail.com>
2021-04-25 02:01:17 +0600

    update box

--- src/components/box.vue
+++ src/components/box.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>sdgdgfnmgds</div>
+  <div>
+    <img src="https://i.ibb.co/LkqYrdg/ezgif-6-187369f41544.gif">
+    <img src="https://i.ibb.co/LkqYrdg/ezgif-6-187369f41544.gif">
+    <img src="https://i.ibb.co/LkqYrdg/ezgif-6-187369f41544.gif">
+  </div>
 </template>
+
+<!-- 5% кэшбек, 1500 тг, +75 exp -->
