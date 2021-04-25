c5e2b5e5e79befcf9749254b66942827f824dd14
Alibek Baiseiit <alibek.baiseiit@gmail.com>
2021-04-25 11:23:13 +0600

    Изменил логотип

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home.vue'
 import login from '@/components/Login.vue'
+import logout from '@/components/Logout.vue'
 import box from '@/components/box.vue'
 
 Vue.use(Router)
@@ -16,6 +17,10 @@ export default new Router ({
         {
             path: '/login',
             component: login
+        },
+        {
+            path: '/logout',
+            component: logout
         },
         {
             path: '/box',
